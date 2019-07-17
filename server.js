61534fbbd72a2612b53218ef4d4ba78f6205d8ef
Cassie Nichols <cassienichols@macbooks-MacBook-Pro.local>
2019-07-16 19:03:33 -0600

    Commit 1 and 2, connected database, can see json in the browser

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+// If you need to delete something 
+//const methodOverride = require('method-override');
+
+const pokemon = require('./pokemon.js')
+app.get('/pokemon', (req, res) => {
+	res.send(pokemon);
+	// res.render('./index.ejs', {
+	// 		Pokemon: pokemon
+		})
+//})
+
+app.get('/pokemon/:index', (req, res) => {
+	res.render('./show.ejs', {
+		eachPokemon: pokemon[req.params.index]
+	})
+});
+
+
+
+
+
+
+
+
+
+
+
+app.get('/', (req, res) => {
+	res.send('Welcome')
+});
+
+
+
+
+
+//listener
+app.listen(3000, () => {
+	console.log('server is running on port 3000')
+});
